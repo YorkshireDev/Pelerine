de710745d43af956216486043d4a69235a4ba097
YorkshireDev <YorkshireDev@protonmail.com>
2022-05-07 23:35:57 +0100

    Create user model

---
+++ Account/User/Model_User.py
@@ -0,0 +1,35 @@
+class ModelUser:
+
+    def __init__(self, username: str, exchange_name: str, coin_pair: str, live_trading: bool):
+
+        self.USERNAME: str = username
+        self.EXCHANGE_NAME: str = exchange_name
+        self.COIN_PAIR: str = coin_pair
+        self.LIVE_TRADING: bool = live_trading
+
+        self.balance: list = [0.0, 0.0]
+
+    def get_username(self):
+
+        return self.USERNAME
+
+    def get_exchange_name(self):
+
+        return self.EXCHANGE_NAME
+
+    def get_coin_pair(self):
+
+        return self.COIN_PAIR
+
+    def is_live_trading(self):
+
+        return self.LIVE_TRADING
+
+    def get_balance(self) -> list:
+
+        return self.balance
+
+    def update_balance(self, base: int, quote: int):
+
+        self.balance[0] = base
+        self.balance[1] = quote
